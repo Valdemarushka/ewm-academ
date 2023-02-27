3914b79c2b8cbc078fc019b4b104746a7bb561f4
paradizer <Valdemarushka89@yandex.ru>
2023-02-27 22:40:40 +0700

    Основной функционал. fix3.

--- ewm-service/src/main/java/ru/practicum/explore_with_me/controllers/admin/CompilationAdminController.java
+++ ewm-service/src/main/java/ru/practicum/explore_with_me/controllers/admin/CompilationAdminController.java
@@ -31,5 +31,4 @@ public class CompilationAdminController {
         compilationService.delete(compId);
     }
 
-    //проверка на глюк сервера
 }
--- ewm-service/src/main/resources/schema.sql
+++ ewm-service/src/main/resources/schema.sql
@@ -12,8 +12,8 @@ CREATE TABLE IF NOT EXISTS categories
 CREATE TABLE IF NOT EXISTS events
 (
     id                 BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    annotation         VARCHAR(255)                NOT NULL,
-    description        VARCHAR(255),
+    annotation         VARCHAR                     NOT NULL,
+    description        VARCHAR,
     title              VARCHAR(1024)               NOT NULL,
     state              VARCHAR(50),
     created_on         TIMESTAMP WITHOUT TIME ZONE,
